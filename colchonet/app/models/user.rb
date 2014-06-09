325eaea2e1d94b97797ad887e726607ec76abfdf
gcoltrobarbosa <gcoltrobarbosa@gmail.com>
2014-06-09 18:05:33 -0300

    Alterações no arquivo Gemfile e encriptação de Password no model User

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -2,15 +2,8 @@ class User < ActiveRecord::Base
 	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
 	validates_presence_of :email, :full_name, :location, :password
-	validates_confirmation_of :password
 	validates_length_of :bio, minimum: 30, allow_blank: false
+	validates_format_of :email, with: EMAIL_REGEXP
 
-	validate :email_format
-
-	# Essa validação pode ser representada da seguinte forma:
-	# validates_format_of :email, with> EMAIL_REGEXP
-
-	def email_format
-		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-	end
+	has_secure_password
 end
