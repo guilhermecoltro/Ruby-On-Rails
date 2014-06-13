acac054cb3eaadb8af074355ae8d777a69f0b740
Guilherme Coltro <gcoltrobarbosa@gmail.com>
2014-06-13 01:39:13 -0300

    Creation of relations between users and rooms && stylization of rooms_index and rooms_show - NEW MIGRATION GENERATED!

--- colchonet/app/models/room.rb
+++ colchonet/app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ActiveRecord::Base
+	belongs_to :user
 	validates_presence_of :title, :location, :description
+
+	scope :most_recent, -> { order('created_at DESC') }
 
 	def complete_name
 		"#{title}, #{location}"
